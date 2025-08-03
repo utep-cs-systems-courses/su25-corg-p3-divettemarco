08bf20fa6467c13a858218caa923f1edc046cf66
divettemarco <divettemarco@outlook.com>
2025-08-03 06:36:16 -0600

    switch interrupt update

--- games/p2InterruptHandler.c
+++ games/p2InterruptHandler.c
@@ -1,33 +1,28 @@
 #include <msp430.h>
-#include "switches.h"
-#include "games.h"
-#include "stateMachines.h"
-
+#include "games.h"          // currState
+#include "switches.h"       // switch input
+#include "stateMachines.h"  // turn_off_update()
 
 /* interrupt for port 2 (switches) */
 void
 __interrupt_vec(PORT2_VECTOR) Port_2(){
-  // state 1: toggle between tv color test and tv b&w test
+  // state 1: tv color test
   if (P2IFG & SW1){
-    if (currState == STATE_TV){
-      currState == STATE_TV_BW;
-    }else{
-      currState = STATE_TV;
-    }
+    currState = STATE_TV;
     turn_off_update();
   }
-  // state 2: car in motion when led is green
+  // state 2: tv color test B&W
   else if (P2IFG & SW2){
+    currState = STATE_TV_BW;
+    turn_off_update();
+  }
+  // state 3: car in motion until button is pressed to park it when red led is on
+  else if (P2IFG & SW3){
     if (currState == STATE_CAR){
       currState = STATE_PARK;
     }else{
       currState = STATE_CAR;
     }
-    turn_off_update();
-  }
-  // state 3: boom animation every x seconds, play beeping before boom
-  else if (P2IFG & SW3){
-    currState = STATE_BOOM;
     turn_off_update();
   }
   // state 4: SOS, lights blaring, siren
